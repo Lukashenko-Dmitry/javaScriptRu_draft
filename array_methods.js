012898962b84c52a7686209bd8412175d609d1df
Dmitry Lukashenko <88265797+Lukashenko-Dmitry@users.noreply.github.com>
2021-10-25 22:38:01 +0300

    Add files via upload

---
+++ array_methods.js
@@ -0,0 +1,36 @@
+// let arr = [3, 5, 1];
+// let arr2 = [8, 9, 15];
+
+// let merged = [...arr, ...arr2];
+
+// console.log(merged);
+
+
+// ----------------------------------------------------------------------
+// 3-module 2-task
+
+// let arr = [5, 3, 8, 1];
+
+// function filterRange(arr, a, b) {
+//     return arr.filter(item => item >= a && item <= b);
+// }
+
+// let filtered = filterRange(arr, 0, 5);
+// console.log(filtered); // [3,1] (совпадающие значения)
+// console.log(arr); // [5,3,8,1] (без изменений)
+// ---------------------------------------------------------------------------
+
+// 3-module 3-task
+
+function camelize(str) {
+    let result = str.split('-');
+    for (let i = 1; i < result.length; i++) {
+        result[i] = result[i].split('');
+        result[i][0] = result[i][0].toUpperCase();
+        result[i] = result[i].join('');
+    }
+    return result.join('');
+}
+
+let cam = camelize('background-color-farm');
+console.log(cam);
