a4f7e042d8c3a448784c0ec76611291912b24ea6
Dmitry Lukashenko <88265797+Lukashenko-Dmitry@users.noreply.github.com>
2021-10-25 22:38:42 +0300

    Add files via upload

--- array_methods.js
+++ array_methods.js
@@ -22,15 +22,17 @@
 
 // 3-module 3-task
 
-function camelize(str) {
-    let result = str.split('-');
-    for (let i = 1; i < result.length; i++) {
-        result[i] = result[i].split('');
-        result[i][0] = result[i][0].toUpperCase();
-        result[i] = result[i].join('');
-    }
-    return result.join('');
-}
+// function camelize(str) {
+//     let result = str.split('-');
+//     for (let i = 1; i < result.length; i++) {
+//         result[i] = result[i].split('');
+//         result[i][0] = result[i][0].toUpperCase();
+//         result[i] = result[i].join('');
+//     }
+//     return result.join('');
+// }
 
-let cam = camelize('background-color-farm');
-console.log(cam);
+// let cam = camelize('background-color-farm');
+// console.log(cam);
+
+// 3-module 4-task
